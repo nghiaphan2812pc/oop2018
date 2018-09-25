41a8d17820d98819d3c5ac94cca3d41a2f750a46
nghiaphan2812pc <34088937+nghiaphan2812pc@users.noreply.github.com>
2018-09-25 21:27:04 +0700

    nopbtweek2

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,27 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 
     public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        int a0 = 0;
+        int a1 = 1;
+        int an = 1;
+
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            for (int i = 2; i < n; i++)
+            {
+                a0 = a1;
+                a1 = an;
+                an = a0 + a1;
+            }
+        }
+        return an;
+
     }
 }
