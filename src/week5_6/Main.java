e0963d31a7c7b6cda6895191516ea899c214a691
nghiaphan2812pc <34088937+nghiaphan2812pc@users.noreply.github.com>
2018-10-17 15:40:12 +0700

    nopbaitaptuan5

---
+++ src/week5_6/Main.java
@@ -0,0 +1,9 @@
+package week5_6;
+
+public class Main {
+    public static void main(String args[]){
+        Layer layer1  = new Layer();
+        layer1.nhapLayer();
+        layer1.in();
+    }
+}
---
+++ src/week5_6/Shape.java
@@ -0,0 +1,34 @@
+package week5_6;
+
+import java.util.Scanner;
+public class Shape {
+    protected int x;
+    protected int y;
+    Scanner scan = new Scanner(System.in);
+    public void NhapToado(){
+        System.out.println("Nhap toa do lan luot x va y:");
+        x = scan.nextInt();
+        y = scan.nextInt();
+    }
+    public void InToado(){
+        System.out.println("Toa do x:" +x);
+        System.out.println("Toa do y:" +y);
+    }
+    public void ToaDo(int x, int y){
+        this.x = x;
+        this.y =y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
